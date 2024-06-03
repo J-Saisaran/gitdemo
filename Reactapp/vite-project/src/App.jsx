a8411dafd4958c4f8e1dd600ede28086f78eca27
J-Saisaran <saransai027@gmail.com>
2024-06-03 16:39:35 +0530

    updated

--- Reactapp/vite-project/src/App.jsx
+++ Reactapp/vite-project/src/App.jsx
@@ -1,16 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  
+import { useState } from "react"
+export default function App() {
+  //JS starts
 
+  const people = ["Abhishek", "Bala", "Deva", "Harish"];
+
+  const users = [
+    {
+      name: "Abhishek",
+      age: "20",
+      pic: "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg",
+    },
+    {
+      name: "Bala",
+      age: "20",
+      pic: "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg",
+    },
+    {
+      name: "Deva",
+      age: "20",
+      pic: "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg",
+    },
+  ];
+  //JS ends
+  //JSX starts
   return (
-      <div>
-      hello
-      </div>  
-  )
+    <div className="App">
+      <Counter />
+      { {users.map((usr) => (
+        <Welcome name={usr.name} age={usr.age} pic={usr.pic} />
+      ))} }
+    </div>
+  );
+  //JSX ends
 }
 
-export default App
+
+function Counter() {
+  // let like = 10
+  const [like, setLike] = useState(0)
+  console.log("Like updated", like)
+
+  return (
+    <div>
+      <button onClick={() => {
+        setLike(like + 1)
+        console.log(like)
+      }}>👍 {like}</button>
+
+    </div>
+  )
+}
